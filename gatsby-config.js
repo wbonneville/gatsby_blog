d30dd4156f4757e4962d90fafda1fd485fefba04
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-13 11:29:14 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: ``,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A blog where code is written about`,
+    author: `@wesley`,
   },
 
   // styled-component plugin
