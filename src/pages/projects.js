cfcd4bf7ac4b4b70a9252efade648e5b320e801a
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-24 12:18:21 -0500

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -37,13 +37,44 @@ const ProjectsPage = () => (
         </div>
         <div className="column is-12">
           <p>Here's a bunch of projects.</p>
-          <a href="https://github.com/wbonneville/spacewalker">
-            <span>link to repo:</span> a space trading game built with React and
-            Redux
+          <a href="https://spacewalkerproject.netlify.com/">
+            a WIP space trading game built with React and Redux
           </a>
           <br />
-          <Anchor href="https://spacewalkerproject.netlify.com/">
-            link to site
+          <Anchor href="https://github.com/wbonneville/spacewalker">
+            link to repo
+          </Anchor>
+          <br />
+          <br />
+          <a href="https://gallerypx.netlify.com/">
+            a responsive gallery built with React that dynamically renders
+            renders photos using data from a JSON file, or alternatively, any
+            API
+          </a>
+          <br />
+          <Anchor href="https://github.com/wbonneville/gallery">
+            link to repo
+          </Anchor>
+          <br />
+          <br />
+          <a href="http://halflifeintelligence.site/">
+            a fun spoof website with several full pages. project built to
+            demonstrate understanding of CSS, Styled Components, and
+            Flexboxgrid.
+          </a>
+          <br />
+          <Anchor href="https://github.com/wbonneville/react-site">
+            link to repo
+          </Anchor>
+          <br />
+          <br />
+          <a href="https://github.com/wbonneville/react-projects">
+            all other small projects and thoroughly commented code-alongs. lots
+            of code.
+          </a>
+          <br />
+          <Anchor href="https://github.com/wbonneville/react-projects">
+            link to repo
           </Anchor>
         </div>
       </Div>
