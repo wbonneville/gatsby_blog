1ca03661df269efccf4f170b5b5d9758ca008d15
Wesley Bonneville <westazbonneville@gmail.com>
2020-02-14 10:24:25 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import styled from 'styled-components';
 const Div = styled.div`
   position: relative;
   top: 150px;
-  left: 6.2%;
-  width: 60%;
+  /* left: 6.2%;
+  width: 60%; */
 
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -25,6 +25,8 @@ const Div = styled.div`
 `;
 
 const Body = styled.div`
+  margin-left: 6%;
+
   & h1 {
     color: #444444;
     font-size: 34px;
@@ -43,18 +45,21 @@ const Body = styled.div`
   }
 `;
 
-const Test = styled.div``;
+const Test = styled.div`
+  margin-left: 6%;
+  margin-right: -6%;
+`;
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Wesley's Blog " />
       <div>
-        <Div className="columns">
-          <Test className="column is-5">
+        <Div className="columns is-tablet">
+          <Test className="column is-one-quarter ">
             <Menu />
           </Test>
-          <Body className="column is-11">
+          <Body className="column is-7 is-10-mobile">
             <h1>
               Hello, I'm <span className="name">Wesley.</span> I'm a Front-End
               React Developer.{' '}
